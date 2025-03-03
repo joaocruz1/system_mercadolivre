8c5bf9ebb6e09e249db89ece93a239858015854e
João Cruz <joaovcruz50@gmail.com>
2025-03-03 17:59:10 -0300

    update_ErrorTokenRefresh

--- data/dataid.py
+++ data/dataid.py
@@ -1,3 +1,1 @@
 spreadsheet_id = "1ITHQ_TSYm1omCJRG2btX8UPWyK2PCVX-tYiBMCm05YI" #id da planilha de token
-
-spreadsheet_id2 = "1w7iwpUz0qoqJiMPFa_35o4kJ4ASvit_ICu7X_R07HS8" #id da planilha de usuários
